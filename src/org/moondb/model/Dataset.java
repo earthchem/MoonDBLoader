64d733ecca7032617a0a96cf9d785071a678a26a
Peng <spark.ji@gmail.com>
2017-09-06 11:37:23 -0500

    Update class Dataset

--- src/org/moondb/model/Dataset.java
+++ src/org/moondb/model/Dataset.java
@@ -1,5 +1,36 @@
 package org.moondb.model;
-
+/*
+ * MoonDB(table:dataset)	Template(sheet:TABLE_TITLE)	Java(Class:Dataset)	
+ * dataset_type											datasetType
+ * dataset_code											datasetCode
+ * dataset_title			TABLE_TITLE					datasetTitle
+ */
 public class Dataset {
-
+	private String datasetType;
+	private String datasetCode;
+	private String datasetTitle;
+	
+	public String getDatasetType() {
+		return datasetType;
+	}
+	
+	public void setDatasetType(String datasetType) {
+		this.datasetType = datasetType;
+	}
+	
+	public String getDatasetCode() {
+		return datasetCode;
+	}
+	
+	public void setDatasetCode(String datasetCode) {
+		this.datasetCode = datasetCode;
+	}
+	
+	public String getDatasetTitle() {
+		return datasetTitle;
+	}
+	
+	public void setDatasetTitle(String datasetTitle) {
+		this.datasetTitle = datasetTitle;
+	}
 }
