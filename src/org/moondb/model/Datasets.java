66288a97c26746fdb8cf93840962288a12f92cff
Peng <spark.ji@gmail.com>
2017-09-08 01:01:53 -0500

    Add class Datasets

---
+++ src/org/moondb/model/Datasets.java
@@ -0,0 +1,15 @@
+package org.moondb.model;
+
+import java.util.List;
+
+public class Datasets {
+	private List<Dataset> datasets;
+	
+	public List<Dataset> getDatasets() {
+		return datasets;
+	}
+
+	public void setDatasets(List<Dataset> datasets) {
+		this.datasets = datasets;
+	}
+}
