3c29dc6a373394440c6c9599ce2548621101f163
Peng <spark.ji@gmail.com>
2017-09-11 12:15:18 -0500

    Add class Methods

---
+++ src/org/moondb/model/Methods.java
@@ -0,0 +1,18 @@
+package org.moondb.model;
+
+/*
+ * Collection of Method
+ */
+import java.util.List;
+
+public class Methods {
+	private List<Method> methods;
+	
+	public List<Method> getMethods() {
+		return methods;
+	}
+
+	public void setMethods(List<Method> methods) {
+		this.methods = methods;
+	}
+}
