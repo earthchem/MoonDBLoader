e5e4f337ea9450ec2c68eb3a93fcecc66bce559d
Peng <spark.ji@gmail.com>
2017-09-11 15:23:45 -0500

    add method getLastCellNum()

--- src/org/moondb/parser/XlsParser.java
+++ src/org/moondb/parser/XlsParser.java
@@ -1,9 +1,26 @@
 package org.moondb.parser;
 
 
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.ss.usermodel.Cell;
 
 public class XlsParser {
+	//Return the last cell number of the specific row in the sheet by the ending character
+	public static Integer getLastCellNum(HSSFSheet sheet, Integer rowNum) {
+		Integer lastCellNum = null;
+		
+		Iterator<Cell> cellIterator = sheet.getRow(rowNum).cellIterator();
+		while(cellIterator.hasNext()) {	
+			Cell cell = cellIterator.next();
+			if(cellToString(cell).trim().equals("-1.0")) {
+				lastCellNum = cell.getColumnIndex();
+				break;
+			}
+		}
+		return lastCellNum;
+	}
 	
 	//convert value of cell to String
 	public static String cellToString(Cell cell) {  
