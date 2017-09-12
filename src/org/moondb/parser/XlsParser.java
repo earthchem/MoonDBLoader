022549a37f4d7774c92a438e20c80b498eb67414
Peng <spark.ji@gmail.com>
2017-09-11 23:00:40 -0500

    rename cellToString to getCellVauleString

--- src/org/moondb/parser/XlsParser.java
+++ src/org/moondb/parser/XlsParser.java
@@ -1,8 +1,7 @@
 package org.moondb.parser;
 
 
-import java.util.Iterator;
-
+import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.ss.usermodel.Cell;
 
@@ -10,33 +9,42 @@ public class XlsParser {
 	//Return the last cell number of the specific row in the sheet by the ending character
 	public static Integer getLastCellNum(HSSFSheet sheet, Integer rowNum) {
 		Integer lastCellNum = null;
+		HSSFRow row = sheet.getRow(rowNum);
 		
-		Iterator<Cell> cellIterator = sheet.getRow(rowNum).cellIterator();
-		while(cellIterator.hasNext()) {	
-			Cell cell = cellIterator.next();
-			if(cellToString(cell).trim().equals("-1.0")) {
-				lastCellNum = cell.getColumnIndex();
-				break;
+		String value;
+		for(int i=row.getFirstCellNum(); i<row.getLastCellNum(); i++) {	
+			value = getCellValueString(row.getCell(i));
+			if( value != null) {
+				if(value.equals("-1.0")) {
+					lastCellNum = i;
+					break;
+				}
 			}
 		}
 		return lastCellNum;
 	}
 	
 	//convert value of cell to String
-	public static String cellToString(Cell cell) {  
-	    Object result = null;
-	    switch (cell.getCellTypeEnum()) {
-
+	public static String getCellValueString(Cell cell) {  
+	    String result = null;
+	    if (cell != null) {
+		    switch (cell.getCellTypeEnum()) {
+		    case BOOLEAN:
+		    	result = Boolean.toString(cell.getBooleanCellValue()).trim();
 	        case NUMERIC: // numeric value in Excel
-	            result = cell.getNumericCellValue();
+	            result = Double.toString(cell.getNumericCellValue()).trim();
 	            break;
 	        case STRING: // String Value in Excel 
-	            result = cell.getStringCellValue();
+	            result = cell.getStringCellValue().trim();
 	            break;
-	        default:  
-	            throw new RuntimeException("There is no support for this type of cell");                        
+	        default:   //return other type as NULL
+	        	result = null;
+	            //throw new RuntimeException("There is no support for this type of cell");                        
+		    }	    	
+	    } else {
+	    	result = null;
 	    }
 
-	    return result.toString();
+	    return result;
 	}
 }
