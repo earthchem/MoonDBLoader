dca9b5a99a327a1e796b459b37ff004feb3c98cc
Peng <spark.ji@gmail.com>
2017-09-18 02:09:45 -0400

    Add class SampleResult, SampleResults

---
+++ src/org/moondb/model/SampleResult.java
@@ -0,0 +1,206 @@
+package org.moondb.model;
+
+import java.util.List;
+
+public class SampleResult {
+	
+	private int datasetNum;
+	private int samplingFeatureNum;
+	private int[] variableNums;
+	private int[] methodNums;
+	private int[] unitNums;
+	private List<ChemistryResult> chemistryResults;
+	private String analysisComment;
+	private String calcAvge;
+	private String replicatesCount;
+	private String material;
+	private String mineral;
+	private String grain;
+	private String rimCore;
+	private String minerialSize;
+	private String spotId;
+	private String inclusionType;
+	private String inclusionMineral;
+	private String hostMineral;
+	private String hostRock;
+	private String inclusionSize;
+	private String heated;
+	private String temperature;
+	
+	public int getDatasetNum() {
+		return datasetNum;
+	}
+	
+	public void setDatasetNum(int datasetNum) {
+		this.datasetNum = datasetNum;
+	}
+	
+	public int getSamplingFeatureNum() {
+		return samplingFeatureNum;
+	}
+	
+	public void setSamplingFeatureNum(int samplingFeatureNum) {
+		this.samplingFeatureNum = samplingFeatureNum;
+	}
+	
+	public int[] getVariableNums() {
+		return variableNums;
+	}
+	
+	public void setVariableNums(int[] variableNums) {
+		this.variableNums = variableNums;
+	}
+	
+	public int[] getMethodNums() {
+		return methodNums;
+	}
+	
+	public void setMethodNums(int[] methodNums) {
+		this.methodNums = methodNums;
+	}
+	
+	public int[] getUnitNums() {
+		return unitNums;
+	}
+	
+	public void setUnitNums(int[] unitNums) {
+		this.unitNums = unitNums;
+	}
+	
+	
+	public List<ChemistryResult> getChemistryResults() {
+		return chemistryResults;
+	}
+
+	public void setChemistryResults(List<ChemistryResult> chemistryResults) {
+		this.chemistryResults = chemistryResults;
+	}
+	
+	public String getAnalysisComment() {
+		return analysisComment;
+	}
+	
+	public void setAnalysisComment(String analysisComment) {
+		this.analysisComment = analysisComment;
+	}
+	
+	public String getCalcAvge() {
+		return calcAvge;
+	}
+	
+	public void setCalcAvge(String calcAvge) {
+		this.calcAvge = calcAvge;
+	}
+	
+	public String getReplicatesCount() {
+		return replicatesCount;
+	}
+	
+	public void setReplicatesCount(String replicatesCount) {
+		this.replicatesCount = replicatesCount;
+	}
+	
+	public String getMaterial() {
+		return material;
+	}
+	
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	
+	public String getMineral() {
+		return mineral;
+	}
+	
+	public void setMineral(String mineral) {
+		this.mineral = mineral;
+	}
+	
+	public String getGrain() {
+		return grain;
+	}
+	
+	public void setGrain(String grain) {
+		this.grain = grain;
+	}
+	
+	public String getRimCore() {
+		return rimCore;
+	}
+	
+	public void setRimCore(String rimCore) {
+		this.rimCore = rimCore;
+	}
+	
+	public String getMinerialSize() {
+		return minerialSize;
+	}
+	
+	public void setMinerialSize(String minerialSize) {
+		this.minerialSize = minerialSize;
+	}
+	
+	public String getSpotId() {
+		return spotId;
+	}
+	
+	public void setSpotId(String spotId) {
+		this.spotId = spotId;
+	}
+	
+	public String getInclusionType() {
+		return inclusionType;
+	}
+	
+	public void setInclusionType(String inclusionType) {
+		this.inclusionType = inclusionType;
+	}
+	
+	public String getInclusionMineral() {
+		return inclusionMineral;
+	}
+	
+	public void setInclusionMineral(String inclusionMineral) {
+		this.inclusionMineral = inclusionMineral;
+	}
+	
+	public String getHostMineral() {
+		return hostMineral;
+	}
+	
+	public void setHostMineral(String hostMineral) {
+		this.hostMineral = hostMineral;
+	}
+	
+	public String getHostRock() {
+		return hostRock;
+	}
+	
+	public void setHostRock(String hostRock) {
+		this.hostRock = hostRock;
+	}
+	
+	public String getInclusionSize() {
+		return inclusionSize;
+	}
+	
+	public void setInclusionSize(String inclusionSize) {
+		this.inclusionSize = inclusionSize;
+	}
+	
+	public String getHeated() {
+		return heated;
+	}
+	
+	public void setHeated(String heated) {
+		this.heated = heated;
+	}
+	
+	public String getTemperature() {
+		return temperature;
+	}
+	
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
+}
---
+++ src/org/moondb/model/SampleResults.java
@@ -0,0 +1,15 @@
+package org.moondb.model;
+
+import java.util.List;
+
+public class SampleResults {
+	private List<SampleResult> sampleResults;
+	
+	public List<SampleResult> getSampleResults() {
+		return sampleResults;
+	}
+
+	public void setSampleResults(List<SampleResult> sampleResults) {
+		this.sampleResults = sampleResults;
+	}
+}
